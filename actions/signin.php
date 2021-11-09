f65787b4eaeffa7550238f9c09c0f929d64619f7
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-11-09 20:35:18 +0530

    Add files via upload

--- actions/signin.php
+++ actions/signin.php
@@ -5,6 +5,8 @@
 	$id = $_POST['InputID'];
 	$pass = $_POST['InputPassword'];
 
+	
+
 	if ($id == "" && $pass == "") {
 		$_SESSION["status"] = "Empty Values.. Please enter your data.";
 		header('location: ../index.php');
@@ -12,7 +14,7 @@
 		//Get Data, Sql & connect Database. Add database
 	  	require_once("../db/connection.php");
 		//first query (that you want to select)
-		$query_one = "SELECT password FROM tb_users WHERE id = '$id';";
+		$query_one = "SELECT id,password FROM tb_users WHERE id = '$id';";
 		//query execute
 		$result = mysqli_query($conn, $query_one);  
 		//Add while loop for first column data display and after display next column
@@ -20,8 +22,8 @@
 			//data
 			if (mysqli_num_rows($result) == 1) {
 				if($row['password'] == $pass){
-					$_SESSION["id"]= $row['id'];
 					header('location: ../includes/dashboard.php');
+					$_SESSION['id'] = $row['id'];
 				}else{
 					header('location: ../index.php');
 				}
