008b7da9cfd1fe9afb62ac3625ef6a1f721b9f34
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-11-09 20:33:29 +0530

    Add files via upload

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -1,15 +1,35 @@
+<!-- import -->
 <?php 	
 
-if(!isset($_SESSION)){
-	session_start();
-}
+	if(!isset($_SESSION)){
+		session_start();
+	}
 
-if ($_SESSION["id"] == '') {
-	header('location: ../index.php');
-}
+	if ($_SESSION['id'] == '') {
+		//header('location: ../index.php');
+	}
 
-$id = $_SESSION["id"];
+	$id = $_SESSION['id'];
 
-echo "Hello";
+	include 'header_1.php';
 
 ?>
+<!-- import end -->
+
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">SLT-Map</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <form method="POST" action="../actions/signout.php" name="SignoutForm">
+	    <button class="btn btn-outline-danger my-2 my-sm-0" data-toggle="modal" data-target="#SigninModal">
+	    	<i class="bi bi-person-bounding-box"></i> [<?php echo $id; ?>] Signout
+	    </button>
+    </form>
+  </div>
+</nav>
+<!-- navbar end -->
---
+++ includes/header_1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+    <!-- Custerm CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+    <!-- ajax Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
