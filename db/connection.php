2f50c81584c03c9d4eb9b7a61ad636fd71eb4076
Thamidu Anjana <pmatabandara@students.nsbm.ac.lk>
2021-11-09 20:37:54 +0530

    Add files via upload

--- db/connection.php
+++ db/connection.php
@@ -1,9 +1,17 @@
 <?php
 
+  /*$servername = "eu-cdbr-west-01.cleardb.com";
+  $username = "b2a0b161bf5200";
+  $password = "084a841a";
+  $dbname = "heroku_6bafc677b869d27";*/
+  //mysql://b2a0b161bf5200:084a841a@eu-cdbr-west-01.cleardb.com/heroku_6bafc677b869d27?reconnect=true
+  
   $servername = "localhost";
   $username = "root";
   $password = "";
   $dbname = "po_slt_network";
+
+  
 
   // Create connection
   $conn = new mysqli($servername, $username, $password, $dbname);
